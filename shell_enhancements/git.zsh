af0a1c800d8f3f1ea4dfd2c3b2465edfe8b13747
xvenge00 <jawe1222@gmail.com>
2022-04-20 18:48:52 +0200

    git ammend alias

--- shell_enhancements/git.zsh
+++ shell_enhancements/git.zsh
@@ -1,3 +1,12 @@
 
 alias gits='git status'
 alias gitc='git checkout'
+
+GIT_BIN="${GIT_BIN:-$(which git)}"  # when GIT_BIN not declared, nor is git, so it should point to correct git bin
+git() {
+    if [[ "$(echo "$0 $*" | xargs)" == "git ammend" ]]; then
+        ${GIT_BIN} commit --amend --no-edit
+    else
+        ${GIT_BIN} "$@"
+    fi
+}
